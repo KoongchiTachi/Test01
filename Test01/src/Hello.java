ef163f3f92b4f6c95d40b326968634b8fc06be5a
KH <KH@KH-PC>
2020-07-06 11:55:57 +0900

    작업했음

--- Test01/src/Hello.java
+++ Test01/src/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("헬로");
-		System.out.println("하이");
+		System.out.println("hello");
+		System.out.println("hi");
 	}
 }
